4d694374aa44795943247321075662e56c55be6a
IvanKhudyakov <halex7@yandex.ru>
2022-09-16 19:38:23 +0300

    Initial Commit

---
+++ HW3.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW3.1
+//
+//  Created by Иван Худяков on 16.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
